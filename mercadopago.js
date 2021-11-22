e1d35e6aaa3f3cdcb0841179627bbb53401970d3
BrunoCostaPrado <brunocostaprado@gmail.com>
2021-11-22 10:01:11 -0300

    .

--- mercadopago.js
+++ mercadopago.js
@@ -1,28 +1,148 @@
-// SDK do Mercado Pago
-const mercadopago = require ('mercadopago');
-// Adicione as credenciais
+var mercadopago = require("mercadopago");
 mercadopago.configure({
-  access_token: 'PROD_ACCESS_TOKEN'
+  access_token:
+    "TEST-6922520056241217-110421-ab5c70a1cd81a1ac53173f28860bf793-1012711557",
 });
 
+var preference = {
+  items: [
+    {
+      title: "Test",
+      quantity: 1,
+      currency_id: "ARS",
+      unit_price: 10.5,
+    },
+  ],
+};
+const cardForm = mp.cardForm({
+  amount: "100.5",
+  autoMount: true,
+  form: {
+    id: "form-checkout",
+    cardholderName: {
+      id: "form-checkout__cardholderName",
+      placeholder: "Titular do cartão",
+    },
+    cardholderEmail: {
+      id: "form-checkout__cardholderEmail",
+      placeholder: "E-mail",
+    },
+    cardNumber: {
+      id: "form-checkout__cardNumber",
+      placeholder: "Número do cartão",
+    },
+    cardExpirationMonth: {
+      id: "form-checkout__cardExpirationMonth",
+      placeholder: "Mês de vencimento",
+    },
+    cardExpirationYear: {
+      id: "form-checkout__cardExpirationYear",
+      placeholder: "Ano de vencimento",
+    },
+    securityCode: {
+      id: "form-checkout__securityCode",
+      placeholder: "Código de segurança",
+    },
+    installments: {
+      id: "form-checkout__installments",
+      placeholder: "Parcelas",
+    },
+    identificationType: {
+      id: "form-checkout__identificationType",
+      placeholder: "Tipo de documento",
+    },
+    identificationNumber: {
+      id: "form-checkout__identificationNumber",
+      placeholder: "Número do documento",
+    },
+    issuer: {
+      id: "form-checkout__issuer",
+      placeholder: "Banco emissor",
+    },
+  },
+  callbacks: {
+    onFormMounted: (error) => {
+      if (error) return console.warn("Form Mounted handling error: ", error);
+      console.log("Form mounted");
+    },
+    onSubmit: (event) => {
+      event.preventDefault();
 
-// Cria um objeto de preferência
-let preference = {
-    items: [
-      {
-        title: 'Meu produto',
-        unit_price: 100,
-        quantity: 1,
-      }
-    ]
-  };
-  
-  mercadopago.preferences.create(preference)
-  .then(function(response){
-  // Este valor substituirá a string "<%= global.id %>" no seu HTML
-    global.id = response.body.id;
-  }).catch(function(error){
-    console.log(error);
+      const {
+        paymentMethodId: payment_method_id,
+        issuerId: issuer_id,
+        cardholderEmail: email,
+        amount,
+        token,
+        installments,
+        identificationNumber,
+        identificationType,
+      } = cardForm.getCardFormData();
+
+      fetch("/process_payment", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          token,
+          issuer_id,
+          payment_method_id,
+          transaction_amount: Number(amount),
+          installments: Number(installments),
+          description: "Descrição do produto",
+          payer: {
+            email,
+            identification: {
+              type: identificationType,
+              number: identificationNumber,
+            },
+          },
+        }),
+      });
+    },
+    onFetching: (resource) => {
+      console.log("Fetching resource: ", resource);
+
+      // Animate progress bar
+      const progressBar = document.querySelector(".progress-bar");
+      progressBar.removeAttribute("value");
+
+      return () => {
+        progressBar.setAttribute("value", "0");
+      };
+    },
+  },
+});
+var mercadopago = require('mercadopago');
+mercadopago.configurations.setAccessToken("TEST-6922520056241217-110421-ab5c70a1cd81a1ac53173f28860bf793-1012711557");
+
+var payment_data = {
+  transaction_amount: Number(req.body.transactionAmount),
+  token: req.body.token,
+  description: req.body.description,
+  installments: Number(req.body.installments),
+  payment_method_id: req.body.paymentMethodId,
+  issuer_id: req.body.issuer,
+  payer: {
+    email: req.body.email,
+    identification: {
+      type: req.body.docType,
+      number: req.body.docNumber
+    }
+  }
+};
+
+mercadopago.payment.save(payment_data)
+  .then(function(response) {
+    res.status(response.status).json({
+      status: response.body.status,
+      status_detail: response.body.status_detail,
+      id: response.body.id
+    });
+  })
+  .catch(function(error) {
+    res.status(response.status).send(error);
   });
 
-  
+mercadopago.preferences.create(preference);
